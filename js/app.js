72f374599d1c22b26ce04436ba3a52e481ab76d2
Murshedul <murshedbinamir@gmail.com>
2022-03-01 13:18:42 +0600

    phone details loaded and displayed on console

--- js/app.js
+++ js/app.js
@@ -32,9 +32,17 @@ const displayPhones = phones => {
                 <div class="card-body">
                     <h4 class="card-title">${phone.phone_name}</h4>
                     <p class="card-text">Brand: ${phone.brand}</p>
-                    <a href="#" class="btn btn-primary border-0">View Details</a>
+                    <a href="#phone-details" onclick="phoneDetails('${phone.slug}')" class="btn btn-primary border-0">View Details</a>
                 </div>
             </div>
         `
     })
+}
+
+// load phone details
+const phoneDetails = phoneId => {
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data))
 }
